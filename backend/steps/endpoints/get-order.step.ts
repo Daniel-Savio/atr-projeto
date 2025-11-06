7cf9137d8dc7c75bef543851e8fba561ef7df015
Daniel Savio <daniel.savio@treetech.com.br>
2025-11-06 17:43:23 -0300

    First things in the backend with Motia

---
+++ backend/steps/endpoints/get-order.step.ts
@@ -0,0 +1,19 @@
+import { ApiRouteConfig, Handlers } from "motia";
+import { string, z } from "zod";
+
+
+export const config: ApiRouteConfig = {
+    name: 'GetOrder',
+    type: 'api',
+    path: '/orders',
+    method: 'GET',
+    bodySchema: z.object({ string: string().min(1) }),
+    flows: ['OrderManagement'],
+    emits: ['OrderListed']
+}
+
+export const handler: Handlers['GetOrder'] = async (req, { logger }) => {
+
+    logger.info('Get Order', { OrderNumber: "teste" })
+    return { status: 200, body: { message: 'Get Order works!' } }
+}
